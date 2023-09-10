f7b999d2490ec91c3ca3b27aa13ee4bb3a3d0f62
neha kumari <neha.kumari@sanchalansoft.com>
2023-09-10 20:51:28 +0530

    first commit

---
+++ src/mainPage/MainPage.js
@@ -0,0 +1,27 @@
+import React  from "react";
+import Navbar from "../components/navbar/Navbar";
+import Hero from "../components/hero/Hero";
+import Best from "../components/best/Best";
+import Featured from "../components/feature/Featured";
+import Footer from "../components/footer/Footer";
+
+
+function MainPage() {
+  return (
+    <>
+      {/* <Navbar />
+      <Hero />
+      <Best />
+      <Featured />
+      <Footer /> */}
+      <Navbar />
+      <Hero />
+      <Best />
+      <Featured />
+      <Footer/>
+      <p>bfjsvk</p>
+    </>
+  );
+}
+
+export default MainPage;
